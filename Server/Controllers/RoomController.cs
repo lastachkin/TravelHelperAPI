d043f35583956ce2ba06b9552504fff7839ca4cb
Andrei Lastachkin <39839965+lastachkin@users.noreply.github.com>
2021-05-07 11:46:39 +0300

    Work with Room controller

---
+++ Server/Controllers/RoomController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelHelperAPI.Models;
+
+namespace TravelHelperAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class RoomController : Controller
+    {
+        TravelHelperContext dbContext = new TravelHelperContext();
+
+        [HttpPost]
+        public string Post([FromBody] Rooms value)
+        {
+            if (!dbContext.Rooms.Any(room => room.HotelId.Equals(value.HotelId) && room.Type.Equals(value.Type)))
+            {
+                Rooms room = new Rooms();
+                room.Id = value.Id;
+                room.HotelId = value.HotelId;
+                room.Count = value.Count;
+                room.Cost = value.Cost;
+                room.Type = value.Type;
+
+                try
+                {
+                    dbContext.Add(room);
+                    dbContext.SaveChanges();
+                    return JsonConvert.SerializeObject("Room type created");
+                }
+                catch (Exception e)
+                {
+                    return JsonConvert.SerializeObject(e.InnerException.Message);
+                }
+            }
+            else
+            {
+                return JsonConvert.SerializeObject("Room type already exists");
+            }
+        }
+    }
+}
