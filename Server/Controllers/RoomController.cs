e8fcee3461c29614abf1ca3680c2db14d405be61
Andrei Lastachkin <39839965+lastachkin@users.noreply.github.com>
2021-05-11 22:14:42 +0300

    sync

--- Server/Controllers/RoomController.cs
+++ Server/Controllers/RoomController.cs
@@ -4,7 +4,6 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using TravelHelperAPI.Models;
 
 namespace TravelHelperAPI.Controllers
