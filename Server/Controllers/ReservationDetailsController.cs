06a7bfe25d0fd7de8c0f6150d58a8d9259dd2382
Andrei Lastachkin <39839965+lastachkin@users.noreply.github.com>
2021-05-17 23:28:27 +0300

    GET Reservation details

---
+++ Server/Controllers/ReservationDetailsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelHelperAPI.Models;
+
+namespace TravelHelperAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReservationDetailsController : Controller
+    {
+        TravelHelperContext dbContext = new TravelHelperContext();
+
+        [HttpGet]
+        public ReservationsResponse Get(string roomId)
+        {
+            Rooms room = dbContext.Rooms.Where(room => room.Id.Equals(roomId)).FirstOrDefault();
+            Hotels hotel = dbContext.Hotels.Where(hotel => hotel.Id.Equals(room.HotelId)).FirstOrDefault();
+
+            ReservationsResponse response = new ReservationsResponse();
+            response.Title = hotel.Title;
+            response.Type = room.Type;
+            response.City = hotel.City;
+            response.Address = hotel.Address;
+            response.Cost = room.Cost;
+
+            return response;
+        }
+    }
+}
--- Server/ReservationsResponse.cs
+++ Server/ReservationsResponse.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TravelHelperAPI
 {
     public class ReservationsResponse
     {
-        public string Id { get; set; }
-        public string UserId { get; set; }
-        public string RoomId { get; set; }
-        public string Status { get; set; }
-        public string StartDate { get; set; }
-        public string EndDate { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int Cost { get; set; }
     }
 }
